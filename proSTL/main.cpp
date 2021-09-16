6e3ec7c0cdc323ebaf7198654aa2059c309d05f7
Saptarsi Saha <saptarsi.saha@icloud.com>
2021-09-16 09:40:28 +0530

    Initial Commit

---
+++ proSTL/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  proSTL
+//
+//  Created by Saptarsi Saha on 16/09/21.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
